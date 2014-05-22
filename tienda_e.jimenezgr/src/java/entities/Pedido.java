8e932120a3f8eba3ce6a39ff2e8efd9782825690
Shkire <Shkire@Shkire-PC>
2014-05-22 13:36:42 +0200

    Modificada funcion del carrito en controllerInicio (falta acabarla) y añadido metodo precioTotal en Pedido

--- tienda_e.jimenezgr/src/java/entities/Pedido.java
+++ tienda_e.jimenezgr/src/java/entities/Pedido.java
@@ -182,4 +182,11 @@ public class Pedido implements Serializable {
         return "clases.Pedido[ numero=" + numero + " ]";
     }
     
+    public double precioTotal(){
+        double total=0;
+        for (RegistroPedidos registro:registroPedidosCollection){
+            total=total+(registro.getProducto1().getPrecio()*registro.getCantidad());
+        }
+        return total;
+    }
 }
