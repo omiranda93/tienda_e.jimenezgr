3663709971833f48539a787627cc6d9335e67393
Shkire <Shkire@Shkire-PC>
2014-05-23 17:46:47 +0200

    PRUEBALO EDU

--- tienda_e.jimenezgr/src/java/springController/ControllerLogin.java
+++ tienda_e.jimenezgr/src/java/springController/ControllerLogin.java
@@ -36,23 +36,23 @@ public class ControllerLogin {
             session.setMaxInactiveInterval(1800);
             
             if(session.getAttribute("carrito")!=null){
-                Pedido p = new Pedido(1,false,"Carrito","luisin","maijaus","10101");
-                Usuario oscar=dao.getUsuario("o.miranda").get(0);
-                p.setUsuario(oscar);
-                dao.insertarPedido(p);
-//                Pedido p = (Pedido) session.getAttribute("carrito");
-//                List <Pedido> usuarioP = dao.getPedidosUsuario(usuario);
-//                Pedido usuarioCarrito = new Pedido(1, false, "Carrito", "", "", "");
-//                usuarioCarrito.setRegistroPedidosCollection(new ArrayList<RegistroPedidos> ());
-//                for(int i = 0; i<usuarioP.size(); i++){
-//                    if ("Carrito".equalsIgnoreCase(usuarioP.get(i).getEstado())){
-//                        usuarioCarrito = usuarioP.get(i);
-//                    }
-//                }
-//                //NO AUMENTA SI MISMO PRODUCTO
-//                usuarioCarrito.getRegistroPedidosCollection().addAll(p.getRegistroPedidosCollection());
-//                session.setAttribute("carrito", usuarioCarrito);
-//                dao.actualizarPedido(p.getNumero(),usuarioCarrito);
+//                Pedido p = new Pedido(1,false,"Carrito","luisin","maijaus","10101");
+//                Usuario oscar=dao.getUsuario("o.miranda").get(0);
+//                p.setUsuario(oscar);
+//                dao.insertarPedido(p);
+                Pedido p = (Pedido) session.getAttribute("carrito");
+                List <Pedido> usuarioP = dao.getPedidosUsuario(usuario);
+                Pedido usuarioCarrito = new Pedido(1, false, "Carrito", "", "", "");
+                usuarioCarrito.setRegistroPedidosCollection(new ArrayList<RegistroPedidos> ());
+                for(int i = 0; i<usuarioP.size(); i++){
+                    if ("Carrito".equalsIgnoreCase(usuarioP.get(i).getEstado())){
+                        usuarioCarrito = usuarioP.get(i);
+                    }
+                }
+                //NO AUMENTA SI MISMO PRODUCTO
+                usuarioCarrito.getRegistroPedidosCollection().addAll(p.getRegistroPedidosCollection());
+                session.setAttribute("carrito", usuarioCarrito);
+                dao.actualizarPedido(p.getNumero(),usuarioCarrito);
             }
            
             //comprobar cesta
