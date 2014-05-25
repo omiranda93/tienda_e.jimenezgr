76840eb14322f743a8e224876ec4d58a16c7201b
EduAwaSeka <edu.rockero5@gmail.com>
2014-05-25 23:07:37 +0200

    a

--- tienda_e.jimenezgr/src/java/springController/ControllerInicio.java
+++ tienda_e.jimenezgr/src/java/springController/ControllerInicio.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package springController;
 
 import entities.Categoria;
@@ -22,10 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author eduas
- */
+
 @Controller
 @RequestMapping("/Inicio")
 public class ControllerInicio {
